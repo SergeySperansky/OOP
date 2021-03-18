409957c487536c7dd4906f664975aba7029537bd
Sergey Speransky <7773764@gmail.com>
2021-03-18 11:48:50 +0300

    # создание классов: Student, Mentor, Lecture и Reviewer # Класс Mentor - родительский, классы Lecture и Reviewer наследуются от Mentor

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
 class Student:
-    pass
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def add_courses(self, course_name):
+        self.finished_courses.append(course_name)
 
 
 class Mentor:
-    def __init__(self, name, surname, courses):
+    def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.courses = courses
+        self.courses_attached = []
+
 
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname, course):
+        self.name = name
+        self.surname = surname
+        self.courses = []
+        self.grades = {}
+
+    def rate_lector(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 class Reviewer(Mentor):
-    pass
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+best_lecturer = Lecturer('Roman', 'Romanovich', 'Python for 5 days')
+
+best_lecturer.grades = (10, 9, 5, 10)
+
+print(best_lecturer.grades)
